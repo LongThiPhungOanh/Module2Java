0986419802a8aeae0b423e532703e8809d0f1a48
Admin <ying49955@gmail.com>
2023-07-19 09:55:14 +0700

    baiTap

---
+++ src/ThuatToanTimKiem/BaiTapMaxMin/Main.java
@@ -0,0 +1,15 @@
+package ThuatToanTimKiem.BaiTapMaxMin;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào 1 chỗi: ");
+        String input = scanner.nextLine();
+        String[] arr = input.split("");
+        for (int i = 0; i < arr.length; i++) {
+
+        }
+    }
+}
