9885a0105897e461db33b03feaaec56c895c8802
Admin <ying49955@gmail.com>
2023-07-19 10:22:38 +0700

    baiTap

--- src/ThuatToanTimKiem/BaiTap/MaxMin/Main.java
+++ src/ThuatToanTimKiem/BaiTap/MaxMin/Main.java
@@ -5,11 +5,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Nhập vào 1 chỗi: ");
-        String input = scanner.nextLine();
-        String[] arr = input.split("");
-        for (int i = 0; i < arr.length; i++) {
-
+        System.out.println("Enter a string:");
+        String inputString = scanner.nextLine();
+        int[] frequentChar = new int[255];
+        for (int i = 0; i < inputString.length(); i++) {
+            int ascii = (int) inputString.charAt(i);
+            frequentChar[ascii] += 1;
         }
+        int max = 0;
+        char character = (char) 255;
+        for (int j = 0; j < 255; j++) {
+            if (frequentChar[j] > max) {
+                max = frequentChar[j];
+                character = (char) j;
+            }
+        }
+        System.out.println("The most appearing letter is '" + character + "' with a frequency of " + max + " times");
     }
 }
