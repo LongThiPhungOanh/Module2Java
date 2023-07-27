aa8a971999a2a69e04f8180b03191f1b2f5d859a
Admin <ying49955@gmail.com>
2023-07-27 10:37:27 +0700

    baiTap

---
+++ src/Strirng_Regex/BaiTap/PhoneNumber/PhoneNumber.java
@@ -0,0 +1,21 @@
+package Strirng_Regex.BaiTap.PhoneNumber;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PhoneNumber {
+    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//        String regex = "^(0|\\+84)4(\\s|\\.)?((3[2-9])|(5[689])|(7[06-9])|(8[1-689])|(9[0-46-9]))(\\d)(\\s|\\.)?(\\d{3})(\\s|\\.)?(\\d{3})$";
+//        Pattern pattern = Pattern.compile(regex);
+//        boolean check;
+//        do {
+//            System.out.println("Nhập chuỗi: ");
+//            Matcher matcher = pattern.matcher(input.nextLine());
+//            check = matcher.matches();
+//            System.out.println("Đúng rồi");
+//        } while (check);
+//        System.out.println("Sai rồi");
+    }
+}
---
+++ src/Strirng_Regex/BaiTap/Validate/Student.java
@@ -0,0 +1,19 @@
+package Strirng_Regex.BaiTap.Validate;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class Student {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String regex = "^[0-9]{5}$";
+        Pattern pattern = Pattern.compile(regex);
+        boolean check;
+        do {
+            System.out.println("Nhập chuỗi: ");
+            Matcher matcher = pattern.matcher(scanner.nextLine());
+            check = matcher.matches();
+            System.out.println("Đúng rồi");
+        } while (check);
+        System.out.println("Sai rồi");
+    }
+}
